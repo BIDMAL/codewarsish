e460a8970d6e21e8fd7e72068ff091489edc9f84
Dmitry Biryukov <bidmal@mail.ru>
2023-08-21 13:51:28 +0400

    descriptions mostly

--- LeetCode/Python/27-RemoveElement.py
+++ LeetCode/Python/27-RemoveElement.py
@@ -1,4 +1,14 @@
 from typing import List
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of
+    nums are not important as well as the size of nums.
+    Return k.
+
+"""
 
 
 class Solution:
@@ -12,8 +22,8 @@ class Solution:
 
 def test(nums: List[int], val: int, output, result):
     solution = Solution()
-    res = solution.removeElement(nums, val)
-    assert res == output
+    out = solution.removeElement(nums, val)
+    assert out == output
     assert nums == result
 
 
